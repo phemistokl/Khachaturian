38b9e3ab0837c88d7c28fc1546b9e4b931131055
Albert Kumyshev <phemistokl@gmail.com>
2016-09-05 23:14:57 +0300

    maestro

--- js/App.js
+++ js/App.js
@@ -1,44 +1,36 @@
-import React, { Component } from 'react';
-import UserList from './components/UserList';
+import React from 'react';
+
+import datas from '../public/data.json';
+
 import load from './utils/load';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data: null,
-      active: 0,
-      term: ''
+const App = React.createClass({
+  getInitialState() {
+    return {
+      data: null
     };
+  },
 
-    this.loadData();
-  }
-
-  loadData() {
-    load(this.props.data).then(users => {
-      this.initialData = JSON.parse(users);
-      this.setState({
-        data: this.initialData
+  componentDidMount() {
+      load(datas).then(users => {
+        this.initialData = JSON.parse(users);
+        this.setState({
+          data: this.initialData
+        });
       });
-    });
-  }
+  },
 
-  updateData(config) {
-    this.setState(config);
-  }
-
-  render() {
+  render: function() {
     return (
       <div className="app container-fluid">
-
-        <div className="row">
-          <div className="col-sm-8 col-md-9 col-lg-10">
-            <UserList data={this.state.data} />
-          </div>
+      <div className="row">
+        <div className="col-sm-8 col-md-9 col-lg-10">
+          {this.state.data}
         </div>
-
+      </div>
       </div>
     );
   }
-}
+});
+
+export default App;
