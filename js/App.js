cea268260cb38c8cdb0121f270d30cfe62f386c8
Albert Kumyshev <phemistokl@gmail.com>
2016-09-18 09:17:22 +0300

    mambo jambo

--- js/App.js
+++ js/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import UserList from './components/UserList';
 import ActiveUser from './components/ActiveUser';
+import Searchbar from './components/Searchbar';
+import Toolbar from './components/Toolbar';
 
 import load from './utils/load';
 
@@ -27,19 +29,36 @@ const App = React.createClass({
   },
 
   render() {
-    return (
-      <div className="app container-fluid">
-        <div className="row">
-          <div className="col-sm-4 col-md-3 col-lg-2">
-            <ActiveUser data={this.state.data} active={this.state.active} />
-          </div>
-          <div className="col-sm-8 col-md-9 col-lg-10">
-            <UserList data={this.state.data} update={this.updateData.bind(this)} />
-          </div>
+  return (
+    <div className="app container-fluid">
+      <div className="row">
+        <div className="col-sm-12">
+          <Searchbar
+            term={this.state.term}
+            data={this.initialData}
+            update={this.updateData.bind(this)}
+          />
         </div>
       </div>
-    );
-  }
+
+      <div className="row">
+        <div className="col-sm-12">
+          <Toolbar initialData={this.initialData} data={this.state.data} update={this.updateData.bind(this)} />
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-sm-4 col-md-3 col-lg-2">
+          <ActiveUser data={this.state.data} active={this.state.active} />
+        </div>
+        <div className="col-sm-8 col-md-9 col-lg-10">
+          <UserList data={this.state.data} update={this.updateData.bind(this)} />
+        </div>
+      </div>
+
+    </div>
+  );
+}
 });
 
 export default App;
